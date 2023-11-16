a8d21414db307cbb1e8627d89ec8e8d666061c38
mersad <mersad_998@yahoo.com>
2023-11-16 17:01:06 +0330

    feat: create app layout

---
+++ src/components/appLayout/AppLayoutStyles.ts
@@ -0,0 +1,25 @@
+import makeStyles from '@mui/styles/makeStyles';
+
+import type { Theme } from '@mui/material';
+
+export const useStyles = makeStyles<Theme>(() => ({
+  container: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+  },
+
+  headerAndContentContainer: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'row',
+    overflow: 'hidden',
+  },
+
+  contentContainer: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+    overflow: 'hidden',
+  },
+}));
