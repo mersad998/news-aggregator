999b8f6ff09e836a9ef50855aeb48e17a9752cc9
mersad <mersad_998@yahoo.com>
2023-11-19 19:04:10 +0330

    feat: design feed card

--- src/components/feedCard/feedCard.style.ts
+++ src/components/feedCard/feedCard.style.ts
@@ -5,6 +5,56 @@ import type { Theme } from '@mui/material';
 export const useStyles = makeStyles<Theme>(() => ({
   container: {
     display: 'flex',
+    flexDirection: 'column',
+    boxSizing: 'border-box',
+    width: '100%',
+    padding: 8,
+    marginTop: 4,
+    border: '1px solid gray',
+    borderRadius: 4,
+    minHeight: 150,
+    position: 'relative',
+
+    '@media (min-width: 800px)': {
+      flexDirection: 'row',
+    },
+  },
+  publisherInfo: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    padding: '2px 4px',
+    borderRight: '1px solid gray',
+    borderBottom: '1px solid gray',
+    borderRadius: 4,
+  },
+  image: {
+    objectFit: 'contain',
+    width: '90%',
+    maxHeight: 250,
+    alignSelf: 'center',
+    margin: '30px 0px 10px 0px',
+    '@media (min-width: 800px)': {
+      flexDirection: 'row',
+    },
+    '&:hover': {
+      boxShadow: '0 0 2px 1px rgba(0, 140, 186, 0.5)',
+    },
+  },
+  summery: {
+    display: 'flex',
+    flexDirection: 'column',
+    flex: '1 1 40%',
+  },
+  content: {
+    display: 'flex',
+    flexDirection: 'column',
+    flex: '1 1 60%',
+  },
+  emptyArea: {
     flex: 1,
   },
 }));
--- src/components/feedCard/feedCard.view.tsx
+++ src/components/feedCard/feedCard.view.tsx
@@ -1,23 +1,57 @@
 import { type FC } from 'react';
 
 import type { FeedCardViewProps } from './feedCard.type';
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
+import { useStyles } from './feedCard.style';
 
 const FeedCardView: FC<FeedCardViewProps> = (props) => {
   const { article } = props;
+  const { author, content, date, description, images, resource, sourceName, title, url } = article;
+
+  const classes = useStyles();
+
+  if (images[0]) {
+    return (
+      <div className={classes.container}>
+        <div className={classes.summery}>
+          <div className={classes.publisherInfo}>
+            <Typography className={classes.publisherInfoText}>
+              {resource} / {sourceName} / {author}
+            </Typography>
+          </div>
+
+          <img src={images[0]} alt={title} className={classes.image} />
+
+          <Typography>{title}</Typography>
+        </div>
+        <div className={classes.content}>
+          <Typography>{description}</Typography>
+          <Typography color="ButtonFace">{date}</Typography>
+          <div className={classes.emptyArea}></div>
+          <Button
+            onClick={() => {
+              window.open(url, '_blank');
+            }}
+          >
+            continue to full page
+          </Button>
+        </div>
+      </div>
+    );
+  }
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'column',
-        width: '100%',
-        height: 200,
-        border: '1px solid gray',
-        marginTop: 4,
-      }}
-    >
-      <Typography>{article.title}</Typography>
+    <div className={classes.container}>
+      <div className={classes.summery}>
+        <Typography className={classes.publisherInfoText}>
+          {resource} / {sourceName} / {author}
+        </Typography>
+
+        <Typography>{title}</Typography>
+      </div>
+      <div className={classes.content}>
+        <Typography>{description}</Typography>
+      </div>
     </div>
   );
 };
